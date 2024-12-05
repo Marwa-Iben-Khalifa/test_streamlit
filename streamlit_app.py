30e500fc27a3b08d8856499fa71a391e250c648c
Marwa Iben khalifa <62248340+Marwa-Iben-Khalifa@users.noreply.github.com>
2024-12-05 20:20:15 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 st.title('Predict Categories 💶')
-with st.expander('Data')
+with st.expander('Data'):
   st.write('**Raw data**')
   df= pd.read_csv('https://raw.githubusercontent.com/Marwa-Iben-Khalifa/data/refs/heads/main/data.csv')
   df
