a2a87060bbacac8fe09beae9c46c685a143048ae
Marwa Iben khalifa <62248340+Marwa-Iben-Khalifa@users.noreply.github.com>
2024-12-12 13:59:39 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,14 @@ with st.expander('Data'):
   st.write('**Raw data**')
   df= pd.read_csv('https://raw.githubusercontent.com/Marwa-Iben-Khalifa/data/refs/heads/main/data.csv')
   df
+
+import pickle
+
+# Charger le modèle et l'encodeur
+with open('model.pkl', 'rb') as model_file:
+    model = pickle.load(model_file)
+with open('encoder.pkl', 'rb') as encoder_file:
+    encoder = pickle.load(encoder_file)
+
+# Titre de l'application
+st.title('Predict Categories 💶')
